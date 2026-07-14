c002f771c7a70cb3d3a05a44e27d74dc8a146403
javiermelendoia-prog <javiermelendoia@gmail.com>
2026-07-14 15:54:52 +0200

    Añade nueva multietiqueta de Monetag (zona 259603)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11285138
+    "zoneId": 11285518
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
